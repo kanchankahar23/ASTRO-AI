0a825fe9562cac9b926b3c6327382ae8abbb21e9
kanchan kahar <kaharkanchan884@gmail.com>
2026-03-26 17:37:06 +0530

    updated

---
+++ client/src/components/Horoscope/Zodiac.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Zodiac = () => {
+    return (
+        <>
+            {/* banner */}
+            <div>
+
+
+            </div>
+            {/* Zodiac */}
+            <div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Zodiac
