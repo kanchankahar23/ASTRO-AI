221e3a41b1ded89c275b15b4985474accffd39b7
kanchan kahar <kaharkanchan884@gmail.com>
2026-03-26 17:35:12 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home"
+import Footer from "./components/Footer/Footer"
+import Horoscope from "./pages/Horoscope"
 
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home";
-import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <Router>
       <Navbar />
-      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/horoscope/daily" element={<Horoscope />} />
+        <Route path="/horoscope/weekly" element={<Horoscope />} />
+        <Route path="/horoscope/yearly" element={<Horoscope />} />
       </Routes>
       <Footer />
     </Router>
