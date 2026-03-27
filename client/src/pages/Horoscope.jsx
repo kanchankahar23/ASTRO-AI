d1151484a0f3e4c74ed6c1ab784d48d5cd19cfab
kanchan kahar <kaharkanchan884@gmail.com>
2026-03-27 15:58:58 +0530

    added horoscope

---
+++ client/src/pages/Horoscope.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Horoscope/Banner'
+import Data from '../components/Horoscope/Data'
+
+const Horoscope = () => {
+  return (
+    <>
+      <Banner />
+      <Data />
+    </>
+  )
+}
+
+export default Horoscope
