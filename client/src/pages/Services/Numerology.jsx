11b9a0bc8f119b7a4f8f58534dc6df6f004665de
kanchan kahar <kaharkanchan884@gmail.com>
2026-03-30 16:42:09 +0530

    feat: add Numerology component for services page

---
+++ client/src/pages/Services/Numerology.jsx
@@ -0,0 +1,11 @@
+const Numerology = () => {
+  return (
+    <div style={{ minHeight: '100vh', background: 'red', color: 'white', 
+                  fontSize: '40px', display: 'flex', alignItems: 'center', 
+                  justifyContent: 'center' }}>
+      NUMEROLOGY PAGE IS WORKING
+    </div>
+  )
+}
+
+export default Numerology
