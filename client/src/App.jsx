58aee2f19e361b0f232ca390e0f6597f06750c03
kanchan kahar <kaharkanchan884@gmail.com>
2026-03-30 16:40:02 +0530

    feat: refactor routing structure by replacing Routes with AppRoutes component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar/Navbar"
-import Footer from "./components/Footer/Footer"
-
-// Pages
-import Home from "./pages/Home"
-import Horoscope from "./pages/Horoscope"
-import Kundali from "./pages/Kundali"
+import { BrowserRouter as Router } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import AppRoutes from './pages/AppRoutes'
 
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Routes>
-
-        {/* Main pages */}
-        <Route path="/" element={<Home />} />
-
-        {/* Single dynamic route instead of 3 separate ones */}
-        <Route path="/horoscope/:type" element={<Horoscope />} />
-
-        {/* Kundali */}
-        <Route path="/kundali" element={<Kundali />} />
-
-        {/* 404 fallback */}
-        <Route path="*" element={
-          <div className="flex flex-col items-center justify-center min-h-screen text-center">
-            <h1 className="text-4xl font-bold text-purple-700">404</h1>
-            <p className="text-gray-500 mt-2">Page not found</p>
-          </div>
-        } />
-
-      </Routes>
+      <AppRoutes />
       <Footer />
     </Router>
   )
