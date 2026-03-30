aa07f54d30b915c6d5d27e594fecc46d3dd45459
kanchan kahar <kaharkanchan884@gmail.com>
2026-03-30 16:41:17 +0530

    feat: update heading text in Banner component for improved clarity

--- client/src/components/Horoscope/Banner.jsx
+++ client/src/components/Horoscope/Banner.jsx
@@ -72,8 +72,8 @@ const Banner = () => {
                 <div className='absolute inset-0 bg-black/50' />
 
                 {/* Heading */}
-                <h1 className="relative z-10 text-2xl md:text-5xl font-bold text-center text-white">
-                    Astro <span className='text-orange-400'>AI</span> Daily Horoscope
+                <h1 className="relative z-10 text-2xl md:text-4xl font-bold text-center text-white">
+                    Astro <span className='text-orange-400'>AI</span> Kundali
                 </h1>
 
             </div>
