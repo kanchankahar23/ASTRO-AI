5b128ee6dc16f1f27f53a4ce1885b268b1d87c41
kanchan kahar <kaharkanchan884@gmail.com>
2026-03-30 16:42:30 +0530

    feat: add Calculator component for services page

---
+++ client/src/pages/Services/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+
+const Calculator = () => {
+  return (
+    
+    <div>Calculator</div>
+  )
+}
+
+export default Calculator
