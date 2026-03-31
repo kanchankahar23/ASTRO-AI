92c220bdd9307f4e01c59d10304b0604ff5f9c55
kanchan kahar <kaharkanchan884@gmail.com>
2026-04-01 00:50:23 +0530

    add

--- client/src/pages/AppRoutes.jsx
+++ client/src/pages/AppRoutes.jsx
@@ -35,6 +35,9 @@ const AppRoutes = () => {
             {/* Contact */}
             <Route path="/contact" element={<Contact />} />
 
+  {/* add these 2 new routes */}
+      <Route path="/sign-in/*" element={<SignInPage />} />
+      <Route path="/sign-up/*" element={<SignUpPage />} />
             {/* 404 */}
             <Route path="*" element={
                 <div className="flex flex-col items-center justify-center min-h-screen text-center">
