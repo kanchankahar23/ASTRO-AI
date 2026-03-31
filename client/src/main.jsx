eede2846cd5575bf6a6044697178cb26cdd0f893
kanchan kahar <kaharkanchan884@gmail.com>
2026-04-01 00:50:45 +0530

    added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={publishableKey}>
     <App />
-  </StrictMode>,
+  </ClerkProvider>
+
+
 )
