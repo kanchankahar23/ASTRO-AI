23e899fdfc27f57b7b5a97f219661a757409307f
kanchan kahar <kaharkanchan884@gmail.com>
2026-04-02 11:09:19 +0530

    updated signin

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/clerk-react'
 
 export default function SignInPage() {
   return (
-    <div style={{ display: 'flex', backgroundColor:'pink', justifyContent: 'center', marginTop: '50px' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
       <SignIn routing="path" path="/sign-in" />
     </div>
   )
