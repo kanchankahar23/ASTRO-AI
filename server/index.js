e0eb21bd1c8d7ba6120bc9c04b2afdb9de6d37e6
kanchan kahar <kaharkanchan884@gmail.com>
2026-04-10 11:38:49 +0530

    update

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const connectDB = require('./config/db')
 const horoscopeRoutes = require('./routes/horoscopeRoutes')
 const kundaliRoutes = require('./routes/kundaliRoutes')
+const kundaliMatchingRoutes = require('./routes/kundaliMatchingRoutes')
+const aiRoutes = require('./routes/aiRoutes')
 
 dotenv.config()
 connectDB()
@@ -15,6 +17,8 @@ app.use(express.json())
 
 app.use('/api/horoscope', horoscopeRoutes)
 app.use('/api/kundali', kundaliRoutes)
+app.use('/api/kundali-matching', kundaliMatchingRoutes)
+app.use('/api/ai', aiRoutes)
 
 app.get('/', (req, res) => {
   res.json({ message: 'AstroBuddy API is running!' })
