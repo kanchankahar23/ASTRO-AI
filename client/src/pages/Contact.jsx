6eda46d1b03ffbfb3f07a2684ddde85f02840d91
kanchan kahar <kaharkanchan884@gmail.com>
2026-04-19 16:15:24 +0530

    update

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -17,6 +17,7 @@ const Contact = () => {
         </p>
       </div>
 
+
       {/* Card */}
       <div className="max-w-4xl mx-auto bg-white rounded-2xl shadow-xl overflow-hidden">
         <div className="flex flex-col md:flex-row">
