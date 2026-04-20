51a04fd85917d4109c4e04052b60ed49def78e1c
kanchan kahar <kaharkanchan884@gmail.com>
2026-04-20 11:53:31 +0530

    added

--- client/src/pages/Horoscope.jsx
+++ client/src/pages/Horoscope.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Banner from '../components/Horoscope/Banner'
 import Data from '../components/Horoscope/Data'
 
 const Horoscope = () => {
+  const [selectedSign, setSelectedSign] = useState(null);
+
   return (
     <>
-      <Banner />
-      <Data />
+      <Banner selectedSign={selectedSign} setSelectedSign={setSelectedSign} />
+      {selectedSign && <Data selectedSign={selectedSign} />}
     </>
   )
 }
